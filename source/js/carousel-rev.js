f76ab0860672f88dee9fcc5d71f749292ae008eb
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-02-18 17:58:12 +0300

    refactoring carousel code

--- source/js/carousel-rev.js
+++ source/js/carousel-rev.js
@@ -5,87 +5,123 @@ let buttonCarouselLeft = document.querySelector('.carousel-rev__button-left');
 let buttonCarouselRight = document.querySelector('.carousel-rev__button-right');
 
 const carousel =
-(windowCarousel, element, buttonCarouselLeft, buttonCarouselRight, widthItem, quantityItem) => {
+  (windowCarousel, element, buttonCarouselLeft, buttonCarouselRight, widthItem, quantityItem) => {
 
-  let shift = 0;
-  let count = 0;
-  let cloneElement;
-  let shiftCloneElementLeft;
-  let shiftCloneElementRight;
+    let shift = 0;
+    let count = 0;
+    let cloneElement;
+    let positionCloneElementLeft;
+    let positionCloneElementRight;
+    //for set active item
+    let countActive = 0;
+    let activeList = element;
+    let notActiveList;
 
-  const createCloneElementLeft = () => {
-    cloneElement = element.cloneNode(true);
-    shiftCloneElementLeft = - widthItem * quantityItem;
-    cloneElement.style.left = shiftCloneElementLeft + 'px';
-    windowCarousel.appendChild(cloneElement);
-    return cloneElement;
-  }
+    const setActiveItem = (activeList) => {
+      const cleanClassOfCollection = (activeList, className) => {
+        for (let i = 0; i < activeList.children.length; i++) {
+          activeList.children[i].classList.remove(className);
+        }
+      }
+      if (notActiveList) {
+        cleanClassOfCollection(notActiveList, 'list-cards__item-current')
+      }
+      cleanClassOfCollection(activeList, 'list-cards__item-current')
 
-  const createCloneElementRight = () => {
-    cloneElement = element.cloneNode(true);
-    shiftCloneElementRight = widthItem;
-    cloneElement.style.left = shiftCloneElementRight + 'px';
-    windowCarousel.appendChild(cloneElement);
-    return cloneElement;
-  }
+      activeList.children[countActive].classList.add('list-cards__item-current');
+    }
+    setActiveItem(activeList);
 
-  let cloneElementLeft = createCloneElementLeft();
-
-  const shiftingButtonLeft = () => {
-    shift -= widthItem;
-    element.style.left = shift + 'px';
-
-    if(cloneElementLeft) {
-      shiftCloneElementLeft -= widthItem;
-      cloneElementLeft.style.left = shiftCloneElementLeft + 'px';
+    const createCloneElementLeft = (shiftingAdditional = 0) => {
+      cloneElement = element.cloneNode(true);
+      positionCloneElementLeft = -widthItem * quantityItem - shiftingAdditional;
+      cloneElement.style.left = positionCloneElementLeft + 'px';
+      windowCarousel.appendChild(cloneElement);
+      return cloneElement;
+    }
+    const createCloneElementRight = () => {
+      cloneElement = element.cloneNode(true);
+      positionCloneElementRight = widthItem + widthItem;
+      cloneElement.style.left = positionCloneElementRight + 'px';
+      windowCarousel.appendChild(cloneElement);
+      return cloneElement;
     }
 
-    if (count == quantityItem - 2) { // почему 2?
-      cloneElementLeft.remove();
-      cloneElementLeft = element;
-      element = createCloneElementRight();
-      element.classList.add('element');
+    let cloneElementLeft = createCloneElementLeft();
 
-      cloneElementLeft.classList.add('clone');
-      shiftCloneElementLeft = shift;
-      shift = widthItem;
-      count = -2;
+    const shiftingLeft = () => {
+      shift -= widthItem;
+      element.style.left = shift + 'px';
+
+      if (cloneElementLeft) {
+        positionCloneElementLeft -= widthItem;
+        cloneElementLeft.style.left = positionCloneElementLeft + 'px';
+      }
+
+      if (count === quantityItem - 3) {
+        cloneElementLeft.remove();
+        cloneElementLeft = element;
+        element = createCloneElementRight();
+        element.classList.add('element');
+
+        cloneElementLeft.classList.add('clone');
+        positionCloneElementLeft = shift;
+        shift = widthItem + widthItem;
+        count = -3;
+      }
+
+      //counting active element
+      countActive++
+      if (countActive === quantityItem) {
+        activeList = element;
+        notActiveList = cloneElementLeft;
+        countActive = 0
+      }
+      setActiveItem(activeList);
+
+      count++;
+      console.log('count: ' + count + '. countActive: ' + countActive);
+    }
+    const shiftingRight = () => {
+      shift += widthItem;
+      element.style.left = shift + 'px';
+
+      if (cloneElementLeft) {
+        positionCloneElementLeft += widthItem;
+        cloneElementLeft.style.left = positionCloneElementLeft + 'px';
+      }
+
+      if (count === -quantityItem + 2) {
+        element.remove();
+        element = cloneElementLeft;
+        shift = positionCloneElementLeft;
+        cloneElementLeft = createCloneElementLeft(widthItem);
+        positionCloneElementLeft -= widthItem - widthItem;
+        count = 2;
+      }
+
+      //counting active element
+      countActive--
+      if (countActive === -1) {
+        countActive = quantityItem - 1;
+        activeList = cloneElementLeft;
+        notActiveList = element;
+      }
+      setActiveItem(activeList);
+
+
+      count--;
+      console.log('count: ' + count + '. countActive: ' + countActive);
     }
 
-    count++;
-    console.log(count);
+    buttonCarouselLeft.addEventListener('click', () => {
+      shiftingLeft();
+    })
+    buttonCarouselRight.addEventListener('click', () => {
+      shiftingRight();
+    })
   }
 
-  const shiftingButtonRight = () =>{
-    shift += widthItem;
-    element.style.left = shift + 'px';
-
-    if(cloneElementLeft) {
-      shiftCloneElementLeft += widthItem;
-      cloneElementLeft.style.left = shiftCloneElementLeft + 'px';
-    }
-
-    if (count == - quantityItem + 1) {
-      element.remove();
-      element = cloneElementLeft;
-      shift = shiftCloneElementLeft;
-      cloneElementLeft = createCloneElementLeft();
-      count = 1;
-    }
-    count--;
-    console.log(count);
-  }
-
-  buttonCarouselLeft.addEventListener('click', () => {
-    shiftingButtonLeft();
-   })
-
-  buttonCarouselRight.addEventListener('click', () => {
-    shiftingButtonRight();
-   })
-
-
-}
 
 carousel(carouselView, carouselList, carouselButtonRight, carouselButtonLeft, 230, 8);
-carousel(windowCarousel, listCarousel, buttonCarouselRight, buttonCarouselLeft, 290, 3);
+carousel(windowCarousel, listCarousel, buttonCarouselRight, buttonCarouselLeft, 290, 4);
